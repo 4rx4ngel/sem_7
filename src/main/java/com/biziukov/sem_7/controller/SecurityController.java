01b408fe134308645d647b94965ade005276cc04
Mikhail_Biziukov <HostProcessor@gmail.com>
2024-11-12 19:48:08 +0300

    2nd commit

---
+++ src/main/java/com/biziukov/sem_7/controller/SecurityController.java
@@ -0,0 +1,44 @@
+package com.biziukov.sem_7.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class SecurityController {
+
+    @GetMapping("/")
+    public String indexPage(final HttpServletRequest request, Principal principal, Model model) {
+        if (principal != null) {
+            model.addAttribute("username", principal.getName());
+        }
+        return new String("index.html");
+    }
+
+    @GetMapping("/private-data")
+    public String privatePage() {
+        return "private.html";
+    }
+
+    @GetMapping("/public-data")
+    public String publicPage() {
+        return "public.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout.html";
+    }
+}
