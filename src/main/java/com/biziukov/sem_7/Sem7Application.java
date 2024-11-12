4ee462313ce6badc668d4c1de6813a3350aa7c40
Mikhail_Biziukov <HostProcessor@gmail.com>
2024-11-12 19:23:02 +0300

    git init

---
+++ src/main/java/com/biziukov/sem_7/Sem7Application.java
@@ -0,0 +1,13 @@
+package com.biziukov.sem_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem7Application.class, args);
+	}
+
+}
